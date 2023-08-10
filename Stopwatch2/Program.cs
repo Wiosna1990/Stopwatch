d0e4a01ee9305b15372dd5568eefe9fee5533009
Wiosna1990 <zanzarah@op.pl>
2023-08-10 22:10:42 +0200

    New Stopwach2

---
+++ Stopwatch2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        DateTime time = new DateTime(2023, 08, 10, 00, 00, 00);
+        Console.WriteLine("Press x to Start");
+
+
+        var input = Console.ReadLine();
+        Console.WriteLine(time);
+        time.AddSeconds(1);
+        Thread.Sleep(1000);
+        while(input == "x")
+
+        {
+            Console.Clear();
+            time = time.AddSeconds(1);
+            Console.WriteLine(time);
+            Thread.Sleep(1000);
+            
+        }
+        
+
+    }
+  
+}
