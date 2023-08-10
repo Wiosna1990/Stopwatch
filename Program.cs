706928b1eca2b9818116b4f1cd4816e5e01d3369
Wiosna1990 <zanzarah@op.pl>
2023-08-10 19:45:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Threading;
+
+class Program
+{
+    static public void Main(String[] args)
+    {
+
+        int h = 0, min = 0, sec = 0;
+
+        Console.WriteLine("Press x to start");
+        var input = Console.ReadLine();
+
+        while (input =="x")
+        {
+            
+                sec++;
+                if (sec == 60)
+                {
+                    sec = 0;
+                    min++;
+                    if (min == 60)
+                    {
+                        min = 0;
+                        h++;
+                    }
+
+                }
+            
+            Console.WriteLine($"{h}:{min}:{sec}");
+            Thread.Sleep(1000);
+
+
+            Console.Clear();
+            
+        }
+
+
+    }
+
+
+}
