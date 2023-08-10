5895160dce7900ae873965cad5deda7204ca22e1
Wiosna1990 <zanzarah@op.pl>
2023-08-10 22:57:14 +0200

    Update

--- Stopwatch3/Program.cs
+++ Stopwatch3/Program.cs
@@ -3,11 +3,12 @@
     
     for(int j=0; j<=59; j++)
     {
+        
         for (int k = 0; k < 100; k++)
         {
             Console.Clear();
             Console.WriteLine($"{i}m {j}s {k}");
-            Thread.Sleep(10);
+            Thread.Sleep(1);
 
         }
 
